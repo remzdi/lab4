3edd8d47a799c32a8d27a1b78424803ddb59559f
remzdi <116484641+remzdi@users.noreply.github.com>
2023-04-21 08:34:03 +0300

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "boolean_vector.h"
+using namespace std;
+void check_boolean_vectors_size(const boolean_vector& B1, const boolean_vector& B2);
+
+// Declare a helper function to get input for a Boolean vector
+boolean_vector get_input_boolean_vector(int vectorNumber);
